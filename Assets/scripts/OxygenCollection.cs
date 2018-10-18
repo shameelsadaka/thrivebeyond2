b0eb70c78486947a6a0b01852a2c30d263a8d43c
= <shameelsadaka@gmail.com>
2018-10-19 01:01:30 +0530

    Sample Commit

--- Assets/scripts/OxygenCollection.cs
+++ Assets/scripts/OxygenCollection.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class OxygenCollection : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D col){
+		print("es");
 		if(col.gameObject.tag =="Oxygen")
 		{Destroy(col.gameObject);
-		  
 		  FindObjectOfType<OxygenCountTracker>().oxygen+=500;
 		}
 
